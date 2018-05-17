cdd76e9ba915cb9c74932a4d1686493a7bf93411
david <david11sone@gmail.com>
2018-05-18 00:15:43 +0530

    dasdas

--- public/ng/directives/header/header.directive.js
+++ public/ng/directives/header/header.directive.js
@@ -13,24 +13,25 @@ app.directive("mainHeader", ['$compile', function ($compile) {
                 $(function () {
                     if ($scope.isToggle) {
                         $('#left-sidebar').animate({ width: "70px" });
-                        $('#main-content-wrapper').animate({ margin: "72px 0px 0 30px" });
+                        $('#main-content-wrapper').animate({ 'margin-left': "70px" });
                         $('.main-header').animate({ margin: "0 71px" });
                         $('.sidebar-footer').hide(500);
                         $('.profile_info').hide(500);
-                        $('#profile_pic_id').removeClass("profile_info");
+                        // $('#profile_pic_id').removeClass("profile_info");
                         $('#profile_pic_id').addClass("no-header-wrapper");
-                        $('.main-header .header-right-content').animate({padding:"0px 0 0 152px"});
+                        $('.main-header .header-right-content').animate({'padding':"0px 0 0 152px"});
 
                         
 
                     } else {
-                        $('#left-sidebar').animate({ width: "230px" });
-                        $('.main-header').animate({ margin: "0 230px" });
-                        $('#main-content-wrapper').animate({ margin: "72px 0px 0 231px" });
+                        $('#main-content-wrapper').animate({'margin-left': "231px" });
+                        $('#left-sidebar').animate({ 'width': "230px" });
+                        $('.main-header').animate({'margin': "0 230px" });
+                        
                         $('.sidebar-footer').show(500);
                         $('.profile_info').show(500);
                         $('#profile_pic_id').removeClass("no-header-wrapper");
-                        $('.main-header .header-right-content').animate({padding:"0px 0 0 0px"});
+                        $('.main-header .header-right-content').animate({'padding':"0px 0 0 0px"});
                     }
                 });
                 $scope.isToggle = !$scope.isToggle;
