98494fc68c9b61f4ed98cc1d3151057cc2f695dd
david <david11sone@gmail.com>
2018-05-19 07:39:40 +0530

    ssssssss

--- dashboard_1/ng/directives/dashboard/dashboard.controller.js
+++ dashboard_1/ng/directives/dashboard/dashboard.controller.js
@@ -3,8 +3,20 @@ var app = angular.module("sbAdminApp");
 app.controller("DashboardController", function ($scope) {
 
     var s = "san";
-    console.log(s.slice(0,1).charAt(0).toUpperCase());
+    console.log(s.slice(0, 1).charAt(0).toUpperCase());
 
-  
-  
+    var playerOne = 120;
+    var playerTwo = 130;
+    var playerThree = 140;
+    var playerFour = 150;
+    var playerFive = 160;
+
+    console.log("Console.log" + " " + playerOne);
+    console.debug("Console.debug" + " " + playerTwo);
+    console.warn("Console.warn" + " " + playerThree);
+    console.info("Console.info" + " " + playerFour);
+    console.error("Console.error" + " " + playerFive);
+
+
+
 });
