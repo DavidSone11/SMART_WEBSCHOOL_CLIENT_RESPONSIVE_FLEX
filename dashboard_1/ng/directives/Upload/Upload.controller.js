b0bd799e7d7fd66df754dbbc37efc7af73315b4c
david <david11sone@gmail.com>
2018-05-21 16:29:44 +0530

    dasdasda

--- dashboard_1/ng/directives/Upload/Upload.controller.js
+++ dashboard_1/ng/directives/Upload/Upload.controller.js
@@ -1,6 +1,6 @@
 
 var app = angular.module("sbAdminApp");
-app.controller('UploadController', ['$scope', '$timeout', '$http','Upload', function ($scope, $timeout, $http,Upload) {
+app.controller('UploadController', ['$scope', '$timeout', '$http', 'Upload', function ($scope, $timeout, $http, Upload) {
     $scope.files = [];
     $scope.isLoading = false;
     $scope.user = {
@@ -35,22 +35,22 @@ app.controller('UploadController', ['$scope', '$timeout', '$http','Upload', func
 
     $scope.mainColor = 1;
 
-    $scope.uploadUser = function(file) {
+    $scope.uploadUser = function (file) {
         file.upload = Upload.upload({
-          url: 'http://localhost:4000/api/v1/userUpload/saveUpload',
-          data: {username: $scope.username, file: file},
+            url: 'http://localhost:4000/api/v1/userUpload/saveUpload',
+            data: { username: 'santosh', file: file },
         });
-    
+
         file.upload.then(function (response) {
-          $timeout(function () {
-            file.result = response.data;
-          });
+            $timeout(function () {
+                file.result = response.data;
+            });
         }, function (response) {
-          if (response.status > 0)
-            $scope.errorMsg = response.status + ': ' + response.data;
+            if (response.status > 0)
+                $scope.errorMsg = response.status + ': ' + response.data;
         }, function (evt) {
-          // Math.min is to fix IE which reports 200% sometimes
-          file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
+            // Math.min is to fix IE which reports 200% sometimes
+            file.progress = Math.min(100, parseInt(100.0 * evt.loaded / evt.total));
         });
 
     }
