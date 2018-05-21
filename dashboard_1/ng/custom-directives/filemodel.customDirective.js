67a092e0089b6721e4a4453255e315044200254f
david <david11sone@gmail.com>
2018-05-21 19:13:18 +0530

    dsdsa

--- dashboard_1/ng/custom-directives/filemodel.customDirective.js
+++ dashboard_1/ng/custom-directives/filemodel.customDirective.js
@@ -1,3 +1,33 @@
 
 var app = angular.module("sbAdminApp");
-//app.directive
+app.directive("fileModel", ['$parse', '$compile', function ($parse, $compile) {
+
+    return {
+        restrict: 'A',
+        compile: function (tElem, tAttrs) {
+            console.log(name + ': compile');
+            return {
+                pre: function (scope, iElem, iAttrs) {
+                    console.log(name + ': pre link');
+                    iElem.bind('change', function(){
+                        console.log("inside pre link");
+                    });
+                },
+                post: function (scope, iElem, iAttrs) {
+                    console.log(name + ': post link');
+                    iElem.bind('change', function(){
+                        console.log("inside post link");
+                    });
+                }
+            }
+        },
+        controller: function ($scope, $http) {
+            $scope.upload = function () {
+                console.log("DASdas");
+            }
+
+        }
+    }
+
+
+}]);
