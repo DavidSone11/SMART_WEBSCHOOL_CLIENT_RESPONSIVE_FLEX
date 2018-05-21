98873f09103d691b37a53aedea9485f3640e24a3
david <david11sone@gmail.com>
2018-05-21 22:57:45 +0530

    daasd

---
+++ dashboard_1/ng/utils/filesize.utils.js
@@ -0,0 +1,9 @@
+'use strict';
+var formatSizeUnits = function (nSize, callBackBefore, callBackAfter, callBackAfterError) {
+
+this.nSize = nSize;
+this.callBackBefore = callBackBefore;
+this.callBackAfter = callBackAfterError;
+
+
+}
